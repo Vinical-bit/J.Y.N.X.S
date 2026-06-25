8e74076d58b6e84074d6940ac9f42f579e341856
Vinical-bit <vinical7.c.cavalcanti@gmail.com>
2026-06-25 11:28:52 -0300

    Create hello.js

---
+++ aula 8/hello.js
@@ -0,0 +1,25 @@
+var name = "vinical";
+
+// se tiver duas variaveis com o mesmo nome, pode dar erro e não sera notificado
+
+let  age = 19;
+
+// notifica se tiver duas variaveis com o mesmo nome
+
+const cpf = "123.456.789-00";
+
+// ela não pode ser alterada, se tentar alterar vai dar erro
+
+let carro = {
+    modelo: "cla 180",
+    ano: "2026",
+    marca: "mercedez"  
+}
+let rg = "usuaruio " + name + " salvo na certidão com a idade " + age;
+
+console.log("Hello world!");
+console.log("My name is " + name + ", i'm " + age + " years old" + " and my cpf is " + cpf);
+console.log(carro);
+console.log(rg);
+
+
